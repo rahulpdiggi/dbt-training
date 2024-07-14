bcede21af9b4dd3cf542685066d06236a0542a8c
rahulpdiggi@gmail.com <rahulpdiggi@gmail.com>
2024-07-14 08:52:56 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,21 @@
+select
+-- from raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE AS orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+-- from raw customer
+c.customername,
+c.segment,
+c.country,
+-- from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid = p.productid
